034273b944e292224f5325039a23125811dfd284
Matt K <mattcs321@gmail.com>
2020-05-11 11:04:52 -0400

    add Auditorium model

--- Models/Auditorium.cs
+++ Models/Auditorium.cs
@@ -7,5 +7,10 @@ namespace Moviebooking.Models
 {
     public class Auditorium
     {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string Address { get; set; }
+        public virtual IList<Seat> Seats { get; set; }
+        public int TotalSeats => this.Seats == null ? 0 : this.Seats.Count;
     }
 }
