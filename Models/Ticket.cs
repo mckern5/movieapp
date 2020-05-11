32010ad37fc251b9bad190fa2b436cbc9b4b4c79
Matt K <mattcs321@gmail.com>
2020-05-11 11:44:20 -0400

    add Ticket Model

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -7,5 +7,10 @@ namespace Moviebooking.Models
 {
     public class Ticket
     {
+
+        public int ShowId { get; set; }
+        public List<int> SeatIds { get; set; }
+        public int Quantity => this.SeatIds == null ? 0 : this.SeatIds.Count;
+
     }
 }
