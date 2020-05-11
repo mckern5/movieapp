fa6cf41e572538bbe379d2c3e12fa9bae9eed273
Matt K <mattcs321@gmail.com>
2020-05-11 11:41:44 -0400

    add semi-colons to Movie model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,11 +8,11 @@ namespace Moviebooking.Models
     public class Movie
     {
         public int ID { get; set; }
-        public string Name { get; set }
-        public string Poster { get; set }
-        public string Description { get;set }
-        public string Cast { get; set }
-        public string Director { get; set }
-        public string Genre { get;set }
+        public string Name { get; set; }
+        public string Poster { get; set; }
+        public string Description { get; set; }
+        public string Cast { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
     }
 }
