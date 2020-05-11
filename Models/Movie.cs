8f953db0745e0fb34159b6fa640b9bd56575f12c
Matt K <mattcs321@gmail.com>
2020-05-11 11:23:20 -0400

    add movie model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,5 +7,12 @@ namespace Moviebooking.Models
 {
     public class Movie
     {
+        public int ID { get; set; }
+        public string Name { get; set }
+        public string Poster { get; set }
+        public string Description { get;set }
+        public string Cast { get; set }
+        public string Director { get; set }
+        public string Genre { get;set }
     }
 }
