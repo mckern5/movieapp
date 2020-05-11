cacf64cc4c63901d1fb3ed9c96598506af8ec2bf
Matt K <mattcs321@gmail.com>
2020-05-11 12:15:06 -0400

    add constructor to Ticket model

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -8,6 +8,11 @@ namespace Moviebooking.Models
     public class Ticket
     {
 
+        public Ticket()
+        {
+            this.SeatIds = new List<int>();
+        }
+
         public int ShowId { get; set; }
         public List<int> SeatIds { get; set; }
         public int Quantity => this.SeatIds == null ? 0 : this.SeatIds.Count;
