1674391cec77ac64c4bc389b606568762541f149
Matt K <mattcs321@gmail.com>
2020-05-11 11:20:37 -0400

    add Booking model

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Moviebooking.Models
 {
     public class Booking
     {
+
+        public Booking()
+        {
+            this.Tickets = new List<Ticket>();
+
+        }
+
+        public int ID { get; set; }
+        public IList<Ticket> Tickets { get; set; }
     }
 }
