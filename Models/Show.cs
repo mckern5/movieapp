144e4891eecfbec31d684da9ea01502e516b2444
Matt K <mattcs321@gmail.com>
2020-05-11 11:38:42 -0400

    add Show model

--- Models/Show.cs
+++ Models/Show.cs
@@ -7,5 +7,13 @@ namespace Moviebooking.Models
 {
     public class Show
     {
+        public int Id { get; set; }
+        public int AuditoriumId { get; set; }
+        public int MovieId { get; set; }
+        public TimeSpan Timing { get; set; }
+        public IList<Seat> AvailableSeats { get; set; }
+        public IList<Seat> BookedSeats { get; set; }
+        public int TotalSeats { get; set; }
+
     }
 }
