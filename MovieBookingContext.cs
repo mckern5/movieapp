bc54541e5788c64737222f03bd579c7f03406e4f
Matt K <mattcs321@gmail.com>
2020-05-11 11:56:40 -0400

    add moviebookingcontext file

---
+++ MovieBookingContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moviebooking.Models;
+
+namespace Moviebooking
+{
+    public class MovieBookingContext:DbContext
+    {
+        public DbSet<Auditorium> Auditoriums { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Show> Shows { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        public MovieBookingContext(DbContextOptions<MovieBookingContext> options):base(options)
+        {
+
+        }
+
+    }
+}
